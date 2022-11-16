096a3474db12cc8848e13bdaa18c1b521f4fe8fe
Mohammad Hosein Majedi nia <102474407+mhmn97@users.noreply.github.com>
2022-11-16 20:16:28 +0330

    Update testlit.py

--- testlit.py
+++ testlit.py
@@ -1,13 +1,16 @@
 import streamlit as st
 
-st.text('Fixed width hiii')
-st.markdown('# titr _Markdown_') # see *
-st.latex(r''' e^{i\pi} + 1 = 0 ''')
-st.code('for i in range(8): foo()')
+#st.text('Fixed width hiii')
+st.markdown('# heading 1 _Markdown_') # see *
+st.markdown('## heading 2 **Markdown**') # see *
+st.title('with title') 
+st.header('with header')
+#st.latex(r''' e^{i\pi} + 1 = 0 ''')
+#st.code('for i in range(8): foo()')
 
-st.sidebar.radio('Select one:', [1, 2])
-b = st.sidebar.slider('slider',0,100)
-st.text(b)
+#st.sidebar.radio('Select one:', [1, 2])
+#b = st.sidebar.slider('slider',0,100)
+#st.text(b)
 
 #aks = st.camera_input("Take a picture",)
 
